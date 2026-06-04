32ffeae23c02e8a5b710812f4b479bc2e161fc61
DanteChoppaXXX <only1dantechoppa@gmail.com>
2026-06-04 19:20:36 +0100

    Updated To New Ticket Page UI

--- src/context/EventContext.jsx
+++ src/context/EventContext.jsx
@@ -12,7 +12,7 @@ const defaultEvent = {
   title: "Verified Resale Ticket",
   image: Tswift,
   tix: 2,
-  date: "Thu • Nov 21 2026 • 7:00 PM",
+  date: "Thu • 21 Nov 2026 • 7:00 PM",
   venue: "Rogers Center",
   seating: "METLIFE GATE",
   seatMap: [
